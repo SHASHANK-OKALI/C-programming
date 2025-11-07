fc092a57efa332f7f1f888ccb38cd78810b17545
Shashank-okali <shashankokali93@gmail.com>
2025-11-07 23:17:29 +0530

    Multiplication program

---
+++ Multiplication.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int a,b,multi;
+    printf("Enter value of a:\n");
+    scanf("%d",&a);
+    printf("Enter value of b:\n");
+    scanf("%d",&b);
+    multi=a*b;
+    printf("The multiplication of a & b is %d",multi);
+    return 0;
+
+}
