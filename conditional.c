498862c8a0caea246ef9cd73098516199301cffe
Shashank-okali <shashankokali93@gmail.com>
2025-11-08 15:17:18 +0530

    code added

--- conditional.c
+++ conditional.c
@@ -25,24 +25,43 @@
 // }
 
 //
+// #include<stdio.h>
+// int main(){
+//     int marks;
+//     printf("Enter student marks:\n");
+//     scanf("%d",&marks);
+
+//     if(marks<30){
+//         printf("Grade is C\n");
+//     }else if(marks>30 && marks<=70){
+//         printf("Grade is B\n");
+//     }else if(marks>70 && marks<=90){
+//         printf("Grade is A\n");
+//     }else if(marks>90 && marks<=100){
+//         printf("Grade is A+\n");
+//     }
+//     else{
+//         printf("Marks are not valid for grade assigning");
+//     }
+// }
+
+//program to check upper or lower case
+
 #include<stdio.h>
 int main(){
-    int marks;
-    printf("Enter student marks:\n");
-    scanf("%d",&marks);
-
-    if(marks<30){
-        printf("Grade is C\n");
-    }else if(marks>30 && marks<=70){
-        printf("Grade is B\n");
-    }else if(marks>70 && marks<=90){
-        printf("Grade is A\n");
-    }else if(marks>90 && marks<=100){
-        printf("Grade is A+\n");
+    char ch;
+    printf("Enter character:\n");
+    scanf("%c",&ch);
+    if(ch>='A' & ch<='Z'){
+       printf("Entered character %c is upper case letter\n",ch);
+    } 
+    else if(ch>='a' & ch<='z')
+    {
+       printf("Entered character %c is lower case letter\n",ch);
+    }else{
+    printf("Entered value is not a character\n");
     }
-    else{
-        printf("Marks are not valid for grade assigning");
-    }
+    return 0;
 }
 
     
