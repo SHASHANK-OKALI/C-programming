011c558fa8cb6dab3de7538fc9f262bbf388f0b8
Shashank-okali <shashankokali93@gmail.com>
2025-11-09 23:09:35 +0530

    code added

--- Day3/pointers1.c
+++ Day3/pointers1.c
@@ -1,9 +1,18 @@
+// #include<stdio.h>
+// int main(){
+//     int age=22;
+//     int *ptr=&age;
+//     int _age=*ptr;
+
+//     printf("%d",_age);
+//     return 0;
+// }
+
 #include<stdio.h>
 int main(){
-    int age=22;
+    int age=28;
     int *ptr=&age;
-    int _age=*ptr;
+    int **pptr=&ptr;
 
-    printf("%d",_age);
-    return 0;
+    printf("%d \n",**pptr);
 }
