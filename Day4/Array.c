262b28a79d60122eb7d9d8f7b5f095ad278c4957
Shashank-okali <shashankokali93@gmail.com>
2025-11-10 16:34:02 +0530

    code added

---
+++ Day4/Array.c
@@ -0,0 +1,43 @@
+//take 3 inputs and add gst and print the output
+// #include<stdio.h>
+// int main(){
+// int price[3];
+
+// printf("Enter first price:");
+// scanf("%d",&price[0]);
+// printf("Enter second price:");
+// scanf("%d",&price[1]);
+// printf("Enter three price:");
+// scanf("%d",&price[2]);
+
+
+// printf("The total of first price is:%f\n",price[0]+(0.18*price[0]));
+// printf("The total of sec price is:%f\n",price[1]+(0.18*price[1]));
+// printf("The total of third price is:%f\n",price[2]+(0.18*price[2]));
+
+// return 0;
+
+
+// }
+
+// #include<stdio.h>
+
+// int countodd(int arr[],int n);
+
+// int main(){
+//     int arr[]={1,2,3,4,5};
+//     printf("%d",countodd(arr,5));
+//     return 0;
+
+// }
+
+// int countodd(int arr[],int n){
+//     int count=0;
+//     for(int i=0; i<n; i++){
+//         if(arr[i]%2!=0){
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+
