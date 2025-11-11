fe68a7ec174edf25decd1057bf1415322a83676b
Shashank-okali <shashankokali93@gmail.com>
2025-11-11 23:01:07 +0530

    code added

--- Day5/strings.c
+++ Day5/strings.c
@@ -45,20 +45,81 @@
 //     return count-1;
 // }
 
+// #include<stdio.h>
+// void salting(char password[]);
+
+// int main(){
+//     char password[100];
+//     scanf("%s",password);
+//     salting(password);
+// }
+
+// void salting(char password[]){
+//     char salt[]="123";
+//     char newpass[200];
+
+//     strcpy(newpass,password);//newpass="test"
+//     strcat(newpass,salt);//newpass="test"+"123";
+//     puts(newpass);
+// }
+
+// #include<stdio.h>
+// void slice(char str[],int n,int m);
+
+// int main(){
+//     char str[]="Helloworld";
+//     slice(str,3,6);
+// }
+
+// void slice(char str[],int n,int m){
+//     char newStr[100];
+//     int j=0;
+//     for(int i = n; i<=m; i++,j++){
+//         newStr[j]=str[i];
+//     }
+//     newStr[j]='\0';
+//     puts(newStr);
+// }
+
+//write a fun to count occurrence of vowels in a string.
+
+// #include<stdio.h>
+// int countvowels(char str[]);
+
+// int main(){
+//     char str[]="shashank";
+//     printf("vowels are : %d",countvowels(str));
+// }
+
+// int countvowels(char str[]){
+//     int count=0;
+//     for(int j=0; str[j]!='\0'; j++){
+//         if(str[j]=='a'  || str[j]=='e' || str[j]=='i' || str[j]=='o' || str[j]=='u')
+//         {
+//        count++;
+//         }
+//     }
+//     return count;
+// }
+
+//check if a given charater is present in a string or not
+
 #include<stdio.h>
-void salting(char password[]);
+
+void checkchar(char str[],char ch);
 
 int main(){
-    char password[100];
-    scanf("%s",password);
-    salting(password);
+    char str[]="Apnacollege";
+    char ch = 'w';
+    checkchar(str,ch);
 }
 
-void salting(char password[]){
-    char salt[]="123";
-    char newpass[200];
-
-    strcpy(newpass,password);//newpass="test"
-    strcat(newpass,salt);//newpass="test"+"123";
-    puts(newpass);
+void checkchar(char str[],char ch){
+    for(int i=0; str[i]!='\0'; i++){
+        if(str[i]==ch){
+            printf("character is present");
+            return;
+        }
+    }
+    printf("character is NOT present:");
 }
