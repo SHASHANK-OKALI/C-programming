1f3e1ce100645a04233d37e2d83fa72c21321cd1
Shashank-okali <shashankokali93@gmail.com>
2025-11-12 17:13:57 +0530

    code added

--- Day6/structures.c
+++ Day6/structures.c
@@ -1,20 +1,77 @@
-#include<stdio.h>
-#include<string.h>
+// #include<stdio.h>
+// #include<string.h>
 
-struct student{
-    int roll;
-    float cgpa;
-    char name[100];
+// struct student{
+//     int roll;
+//     float cgpa;
+//     char name[100];
+// };
+
+// int main(){
+//     struct student s1;
+//     s1.roll = 134;
+//     s1.cgpa = 9.5;
+//     strcpy(s1.name,"shashank");
+
+//     printf("student roll no is %d\n",s1.roll);
+//     printf("student cgpa is %f\n",s1.cgpa);
+//     printf("student name is %s\n",s1.name);
+
+// }
+
+
+#include<stdio.h>
+
+struct  address {
+    int houseno;
+    int block;
+    char city[100];
+    char state[100];
 };
 
 int main(){
-    struct student s1;
-    s1.roll = 134;
-    s1.cgpa = 9.5;
-    strcpy(s1.name,"shashank");
+    struct address adds[5];
 
-    printf("student roll no is %d\n",s1.roll);
-    printf("student cgpa is %f\n",s1.cgpa);
-    printf("student name is %s\n",s1.name);
+    printf("Enter info for person 1:");
+    scanf("%d",&adds[0].houseno);
+    scanf("%d",&adds[0].block);
+    scanf("%s",adds[0].city);
+    scanf("%s",adds[0].state);
+
+    printf("Enter info for person 2:");
+    scanf("%d",&adds[1].houseno);
+    scanf("%d",&adds[1].block);
+    scanf("%s",adds[1].city);
+    scanf("%s",adds[1].state);
+
+    printf("Enter info for person 3:");
+    scanf("%d",&adds[2].houseno);
+    scanf("%d",&adds[2].block);
+    scanf("%s",adds[2].city);
+    scanf("%s",adds[2].state);
+
+    printf("Enter info for person 4:");
+    scanf("%d",&adds[3].houseno);
+    scanf("%d",&adds[3].block);
+    scanf("%s",adds[3].city);
+    scanf("%s",adds[3].state);
+
+    printf("Enter info for person 5:");
+    scanf("%d",&adds[4].houseno);
+    scanf("%d",&adds[4].block);
+    scanf("%s",adds[4].city);
+    scanf("%s",adds[4].state);
+    
+    printAdd(adds[0]);
+    printAdd(adds[1]);
+    printAdd(adds[2]);
+    printAdd(adds[3]);
+    printAdd(adds[4]);
+    return 0;
+
+}
+
+void printAdd(struct address adds){
+    printf("Address is : %d,%d,%s,%s\n",adds.houseno,adds.block,adds.city,adds.state);
 
 }
