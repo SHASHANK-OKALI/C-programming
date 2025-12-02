aff4742f58e499c0b0f2e7f369575229094a9939
Shashank-okali <shashankokali93@gmail.com>
2025-12-02 23:44:39 +0530

    pratice ex

--- practice.c
+++ practice.c
@@ -216,7 +216,7 @@ int main() {
     return 0;
 }*/
 
-#include <stdio.h>
+/*#include <stdio.h>
 
 void quicksort(int arr[], int low, int high) {
     if (low < high) {
@@ -262,5 +262,39 @@ int main() {
     printf("\n");
 
     return 0;
+}*/
+
+#include <stdio.h>
+
+int main() {
+    int arr[50], n, i, j, temp;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Bubble Sort
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    printf("Sorted array (Bubble Sort): ");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
 }
+
 
