f90a0797e3b1b5eea46cdc6f95989bd079030ee7
Shashank-okali <shashankokali93@gmail.com>
2025-12-04 23:03:21 +0530

    examples

--- practice.c
+++ practice.c
@@ -264,7 +264,7 @@ int main() {
     return 0;
 }*/
 
-#include <stdio.h>
+/*#include <stdio.h>
 
 int main() {
     int arr[50], n, i, j, temp;
@@ -295,6 +295,52 @@ int main() {
     printf("\n");
 
     return 0;
-}
+}*/
 
+
+#include<stdio.h>
+void merge(int arr[],int left,int mid,int right)
+{
+    int i=left,j=mid+1,k=0;
+    int temp[50];
+
+    while(i<=mid && j<=right){
+        if(arr[i]<arr[j])
+        temp[k++]=arr[i++];
+        else
+        temp[k++]=arr[j++];
+    }
+    while(i<=mid)
+    temp[k++]=arr[i++];
+    while (j<=right)
+    temp[k++]=arr[j++];
+    for(i=left,k=0; i<=right; i++,k++)
+    arr[i]=temp[k];
+}
+void mergesort(int arr[],int left,int right)
+{
+  if(left<right)
+  {
+    int mid = (left+right)/2;
+    mergesort(arr,left,mid);
+    mergesort(arr,mid+1,right);
+    merge(arr,left,mid,right);
+  }
+}
+int main()
+{
+  int arr[50],n;
+  printf("Enter number of elements");
+  scanf("%d",&n);
+  printf("Enter %d elements:\n",n);
+  for(int i=0; i<n; i++)
+  scanf("%d",&arr[i]);
+  mergesort(arr,0,n-1);
+  printf("Sorted Array");
+  for(int i=0; i<n; i++)
+  printf("%d",arr[i]);
+  return 0;
+}
+    
+    
 
