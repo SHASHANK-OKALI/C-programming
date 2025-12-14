a59dc5f33b7c1bf380d675ae142a61efb98bbbf6
Shashank-okali <shashankokali93@gmail.com>
2025-12-14 23:01:53 +0530

    practice

--- practice.c
+++ practice.c
@@ -704,7 +704,7 @@ int main() {
 
 //Quick sort
 
-#include <stdio.h>
+/*#include <stdio.h>
 
 // Function to swap two numbers
 void swap(int *a, int *b) {
@@ -756,7 +756,65 @@ int main() {
     }
 
     return 0;
+}*/
+
+//merge sort
+
+#include <stdio.h>
+
+// Function to merge two halves
+void merge(int arr[], int low, int mid, int high) {
+    int i = low, j = mid + 1, k = 0;
+    int temp[50];
+
+    while(i <= mid && j <= high) {
+        if(arr[i] < arr[j])
+            temp[k++] = arr[i++];
+        else
+            temp[k++] = arr[j++];
+    }
+
+    while(i <= mid)
+        temp[k++] = arr[i++];
+
+    while(j <= high)
+        temp[k++] = arr[j++];
+
+    for(i = low, k = 0; i <= high; i++, k++)
+        arr[i] = temp[k];
 }
+
+// Merge Sort function
+void mergeSort(int arr[], int low, int high) {
+    if(low < high) {
+        int mid = (low + high) / 2;
+        mergeSort(arr, low, mid);
+        mergeSort(arr, mid + 1, high);
+        merge(arr, low, mid, high);
+    }
+}
+
+int main() {
+    int arr[50], n;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    mergeSort(arr, 0, n - 1);
+
+    printf("Sorted array:\n");
+    for(int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
+
 
 
 
