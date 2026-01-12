cb3d2590872e3aadffd79558a758d9722eb7d526
Shashank-okali <shashankokali93@gmail.com>
2026-01-12 14:21:05 +0530

    practice

--- practice.c
+++ practice.c
@@ -1087,17 +1087,38 @@ int main() {
 
 //count number of digits
 
+// #include <stdio.h>
+// int main() {
+//     int n, count = 0;
+//     scanf("%d", &n);
+//     while(n != 0) {
+//         count++;
+//         n /= 10;
+//     }
+//     printf("Digits = %d", count);
+//     return 0;
+// }
+
+//pascal's triangle
+
 #include <stdio.h>
+int fact(int n) {
+    int f = 1;
+    for(int i = 1; i <= n; i++)
+        f *= i;
+    return f;
+}
 int main() {
-    int n, count = 0;
+    int n;
     scanf("%d", &n);
-    while(n != 0) {
-        count++;
-        n /= 10;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j <= i; j++)
+            printf("%d ", fact(i) / (fact(j) * fact(i - j)));
+        printf("\n");
     }
-    printf("Digits = %d", count);
     return 0;
 }
+
 
 
 
