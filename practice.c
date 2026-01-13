c276ed369f8b07b456074e0880355a129273f915
Shashank-okali <shashankokali93@gmail.com>
2026-01-13 23:57:43 +0530

    practice

--- practice.c
+++ practice.c
@@ -1121,23 +1121,39 @@ int main() {
 
 //prime check using prime functions
 
+// #include <stdio.h>
+// int isPrime(int n) {
+//     if(n <= 1) return 0;
+//     for(int i = 2; i <= n/2; i++)
+//         if(n % i == 0)
+//             return 0;
+//     return 1;
+// }
+// int main() {
+//     int n;
+//     scanf("%d", &n);
+//     if(isPrime(n))
+//         printf("Prime");
+//     else
+//         printf("Not Prime");
+//     return 0;
+// }
+
+//gcd using functions
+
 #include <stdio.h>
-int isPrime(int n) {
-    if(n <= 1) return 0;
-    for(int i = 2; i <= n/2; i++)
-        if(n % i == 0)
-            return 0;
-    return 1;
+int gcd(int a, int b) {
+    if(b == 0)
+        return a;
+    return gcd(b, a % b);
 }
 int main() {
-    int n;
-    scanf("%d", &n);
-    if(isPrime(n))
-        printf("Prime");
-    else
-        printf("Not Prime");
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("GCD = %d", gcd(a, b));
     return 0;
 }
+
 
 
 
