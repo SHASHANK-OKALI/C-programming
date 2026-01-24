7237764cd1f4fb5dcb6f37fceeb1ec2e99eeb02d
Shashank-okali <shashankokali93@gmail.com>
2026-01-24 23:14:37 +0530

    practice

--- practice.c
+++ practice.c
@@ -1471,27 +1471,40 @@ int main() {
 
 //Check Number is Palindromic Prime
 
+// #include <stdio.h>
+// int isPrime(int n){
+//     if(n<=1) return 0;
+//     for(int i=2;i<=n/2;i++)
+//         if(n%i==0) return 0;
+//     return 1;
+// }
+// int reverse(int n){
+//     int r=0;
+//     while(n){ r=r*10+n%10; n/=10; }
+//     return r;
+// }
+// int main(){
+//     int n;
+//     scanf("%d",&n);
+//     if(isPrime(n) && reverse(n)==n)
+//         printf("Palindromic Prime");
+//     else
+//         printf("Not Palindromic Prime");
+//     return 0;
+// }
+
+//check perfect cube
+
 #include <stdio.h>
-int isPrime(int n){
-    if(n<=1) return 0;
-    for(int i=2;i<=n/2;i++)
-        if(n%i==0) return 0;
-    return 1;
-}
-int reverse(int n){
-    int r=0;
-    while(n){ r=r*10+n%10; n/=10; }
-    return r;
-}
-int main(){
-    int n;
+int main() {
+    int n,i;
     scanf("%d",&n);
-    if(isPrime(n) && reverse(n)==n)
-        printf("Palindromic Prime");
-    else
-        printf("Not Palindromic Prime");
+    for(i=1;i*i*i<=n;i++);
+    if((i-1)*(i-1)*(i-1)==n) printf("Perfect Cube");
+    else printf("Not Perfect Cube");
     return 0;
 }
+
 
 
 
