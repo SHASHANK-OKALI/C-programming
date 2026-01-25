2a1f1fc6e7a4d52d12264069e845406969603ee0
Shashank-okali <shashankokali93@gmail.com>
2026-01-25 23:38:16 +0530

    practice

--- practice.c
+++ practice.c
@@ -1508,21 +1508,39 @@ int main() {
 
 //Quadratic Equation Roots
 
+// #include <stdio.h>
+// #include <math.h>
+// int main() {
+//     float a,b,c,d,r1,r2;
+//     scanf("%f%f%f",&a,&b,&c);
+//     d=b*b-4*a*c;
+//     if(d>=0){
+//         r1=(-b+sqrt(d))/(2*a);
+//         r2=(-b-sqrt(d))/(2*a);
+//         printf("Roots = %.2f %.2f", r1, r2);
+//     } else {
+//         printf("Imaginary Roots");
+//     }
+//     return 0;
+// }
+
+//Identity matrix check
+
 #include <stdio.h>
-#include <math.h>
 int main() {
-    float a,b,c,d,r1,r2;
-    scanf("%f%f%f",&a,&b,&c);
-    d=b*b-4*a*c;
-    if(d>=0){
-        r1=(-b+sqrt(d))/(2*a);
-        r2=(-b-sqrt(d))/(2*a);
-        printf("Roots = %.2f %.2f", r1, r2);
-    } else {
-        printf("Imaginary Roots");
-    }
+    int n,a[10][10],flag=1;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+        for(int j=0;j<n;j++){
+            scanf("%d",&a[i][j]);
+            if((i==j && a[i][j]!=1) || (i!=j && a[i][j]!=0))
+                flag=0;
+        }
+    if(flag) printf("Identity Matrix");
+    else printf("Not Identity");
     return 0;
 }
+
 
 
 
