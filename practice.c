4a969dcffcbd915647d29f4b26dda9066991dcca
Shashank-okali <shashankokali93@gmail.com>
2026-01-25 23:33:11 +0530

    practice

--- practice.c
+++ practice.c
@@ -1495,13 +1495,32 @@ int main() {
 
 //check perfect cube
 
+// #include <stdio.h>
+// int main() {
+//     int n,i;
+//     scanf("%d",&n);
+//     for(i=1;i*i*i<=n;i++);
+//     if((i-1)*(i-1)*(i-1)==n) printf("Perfect Cube");
+//     else printf("Not Perfect Cube");
+//     return 0;
+// }
+
+
+//Quadratic Equation Roots
+
 #include <stdio.h>
+#include <math.h>
 int main() {
-    int n,i;
-    scanf("%d",&n);
-    for(i=1;i*i*i<=n;i++);
-    if((i-1)*(i-1)*(i-1)==n) printf("Perfect Cube");
-    else printf("Not Perfect Cube");
+    float a,b,c,d,r1,r2;
+    scanf("%f%f%f",&a,&b,&c);
+    d=b*b-4*a*c;
+    if(d>=0){
+        r1=(-b+sqrt(d))/(2*a);
+        r2=(-b-sqrt(d))/(2*a);
+        printf("Roots = %.2f %.2f", r1, r2);
+    } else {
+        printf("Imaginary Roots");
+    }
     return 0;
 }
 
