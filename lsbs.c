b4b1108391ab51d7073d83a9cd068c2ac5d786d6
Shashank-okali <shashankokali93@gmail.com>
2026-02-02 23:58:02 +0530

    practice

--- lsbs.c
+++ lsbs.c
@@ -181,52 +181,111 @@
 
 //merge sort
 
-#include<stdio.h>
-void merge(int arr[],int left,int mid,int right)
-{
-    int i=left,j=mid+1,k=0;
-    int temp[50];
-    while(i<=mid && j<=right)
-    {
-        if(arr[i]<arr[j])
-        temp[k++]=arr[i++];
-        else
-        temp[k++]=arr[j++];
+// #include<stdio.h>
+// void merge(int arr[],int left,int mid,int right)
+// {
+//     int i=left,j=mid+1,k=0;
+//     int temp[50];
+//     while(i<=mid && j<=right)
+//     {
+//         if(arr[i]<arr[j])
+//         temp[k++]=arr[i++];
+//         else
+//         temp[k++]=arr[j++];
 
-    }
-    while(i<=mid)
-    temp[k++]=arr[i++];
-    while(j<=right)
-    temp[k++]=arr[j++];
-    for(int i=left,k=0;i<=right;i++,k++)
-    arr[i]=temp[k];
+//     }
+//     while(i<=mid)
+//     temp[k++]=arr[i++];
+//     while(j<=right)
+//     temp[k++]=arr[j++];
+//     for(int i=left,k=0;i<=right;i++,k++)
+//     arr[i]=temp[k];
+// }
+
+// void mergeSort(int arr[],int left,int right)
+// {
+//     if(left<right)
+//     {
+//         int mid=(left+right)/2;
+//         mergeSort(arr,left,mid);
+//         mergeSort(arr,mid+1, right);
+//         merge(arr,left,mid,right);
+
+//     }
+// }
+
+// int main(){
+//     int arr[50],n;
+//     printf("Enter number of elements");
+//     scanf("%d",&n);
+//     printf("Enter %d elements:\n",n);
+//     for(int i=0; i<n; i++)
+//     scanf("%d",&arr[i]);
+//     mergeSort(arr,0,n-1);
+//     printf("Sorted array:");
+//     for(int i=0; i<n; i++)
+//     printf("%d",arr[i]);
+//     return 0;
+
+// }
+    
+
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node{
+    int data;
+    struct Node*next;
 }
 
-void mergeSort(int arr[],int left,int right)
-{
-    if(left<right)
-    {
-        int mid=(left+right)/2;
-        mergeSort(arr,left,mid);
-        mergeSort(arr,mid+1, right);
-        merge(arr,left,mid,right);
-
+void displayNodes(struct Node*head){
+    printf("Linked List Nodes:");
+    while(head!=NULL){
+        printf("%d",head->data);
+        head = head->next;
     }
+    printf("\n");
+}
+
+int countNodes(struct Node*head){
+    int count=0;
+    while(head!=NULL)
+    {
+       count++;
+       head=head->next;
+    }
+    return count;
 }
 
 int main(){
-    int arr[50],n;
-    printf("Enter number of elements");
-    scanf("%d",&n);
-    printf("Enter %d elements:\n",n);
-    for(int i=0; i<n; i++)
-    scanf("%d",&arr[i]);
-    mergeSort(arr,0,n-1);
-    printf("Sorted array:");
-    for(int i=0; i<n; i++)
-    printf("%d",arr[i]);
-    return 0;
 
+    struct Node*head=NULL;
+
+    head=(struct Node*)malloc(sizeof(struct Node));
+    head->data=1;
+    head->next=NULL;
+
+    struct Node* second = (struct Node*)malloc(sizeof(struct Node));
+    second ->data=2;
+    second ->next=NULL;
+    head->next=second;
+
+    struct Node* third = (struct Node*)malloc(sizeof(structNodes));
+    third ->data=3;
+    third ->next=NULL;
+    second->next=third;
+
+    displayNodes(head);
+
+    int nodeCount = countNodes(head);
+    printf("Number of nodes in the linked list : %d\n",nodeCount);
+
+    while(head!=NULL)
+    {
+        struct Node*temp=head;
+        head=head->next;
+        free(temp);
+    }
+    return 0;
 }
-    
 
