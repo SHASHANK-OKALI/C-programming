5bed066e42e260626918ad490a4c516c614e2e0d
Shashank-okali <shashankokali93@gmail.com>
2026-02-10 23:34:17 +0530

    practice

--- practice.c
+++ practice.c
@@ -1807,6 +1807,61 @@ int main() {
 //     return 0;
 // }
 
+//
+
+// #include <stdio.h>
+// int main() {
+//     double a, b, product;
+//     printf("Enter two numbers: ");
+//     scanf("%lf %lf", &a, &b);  
+ 
+    // Calculating product
+    // product = a * b;
+
+    // %.2lf displays number up to 2 decimal point
+//     printf("Product = %.2lf", product);
+    
+//     return 0;
+
+
+//simple singly linked list
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+void printList(struct Node* n) {
+    while (n != NULL) {
+        printf("%d -> ", n->data);
+        n = n->next;
+    }
+    printf("NULL\n");
+}
+
+int main() {
+    struct Node* head = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* second = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* third = (struct Node*)malloc(sizeof(struct Node));
+
+    head->data = 10; 
+    head->next = second; 
+
+    second->data = 20;
+    second->next = third;
+
+    third->data = 30;
+    third->next = NULL;
+
+    printList(head);
+    return 0;
+}
+
+
+
 
 
 
