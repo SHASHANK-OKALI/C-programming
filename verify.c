e90c185ecc40e77dba883df4ad10316236779bb7
Shashank-okali <shashankokali93@gmail.com>
2026-02-14 22:05:26 +0530

    commit

--- verify.c
+++ verify.c
@@ -20,19 +20,37 @@
 //convert degree to radians
 
 
+// #include <stdio.h>
+// #include <math.h>
+
+// int main() {
+//     double deg, rad;
+
+//     scanf("%lf", &deg);
+//     rad = deg * M_PI / 180;
+//     printf("Radians = %.2lf\n", rad);
+
+//     scanf("%lf", &rad);
+//     deg = rad * 180 / M_PI;
+//     printf("Degrees = %.2lf", deg);
+
+//     return 0;
+// }
+
+//find hypotenuse
+
 #include <stdio.h>
 #include <math.h>
 
 int main() {
-    double deg, rad;
+    double adj, deg, rad;
+    scanf("%lf %lf", &adj, &deg);
 
-    scanf("%lf", &deg);
     rad = deg * M_PI / 180;
-    printf("Radians = %.2lf\n", rad);
 
-    scanf("%lf", &rad);
-    deg = rad * 180 / M_PI;
-    printf("Degrees = %.2lf", deg);
+    double hyp = adj / cos(rad);
+
+    printf("Hypotenuse = %.2lf", hyp);
 
     return 0;
 }
