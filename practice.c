f4ea15dc23a1d99ca7d15b5bea7e12b19bdb47b5
Shashank-okali <shashankokali93@gmail.com>
2026-02-19 22:10:37 +0530

    practice

--- practice.c
+++ practice.c
@@ -2089,72 +2089,256 @@ int main() {
 //LSBS
 
 
+// #include<stdio.h>
+// int main()
+// {
+//     int i,n,key,choice;
+//     printf("Enter number of elements : \n");
+//     scanf("%d",&n);
+
+//     int arr[n];
+
+//     printf("Enter %d elements:\n",n);
+//     for(i=0; i<n; i++)
+//     {
+//         scanf("%d",&arr[i]);
+//     }
+
+//     printf("-----search choice-----");
+//     printf("1.Linear search");
+//     printf("2.Binary search");
+//     printf("Enter your choice:");
+//     scanf("%d",&choice);
+//     printf("Enter element  to serach :");
+//     scanf("%d",&key);
+
+//     if(choice==1)
+//     {
+//         for(i=0; i<n; i++)
+//         {
+//             if(arr[i]==key)
+//             {
+//                 printf("Element found at position %d",i+1);
+//                 return 0;
+//             }
+//         }
+//         printf("Element not found");
+//     }
+
+
+// else if(choice==2)
+// {
+//     int low=0,high=n-1,mid;
+//     while(low<=high)
+//     {
+//         mid=(low+high)/2;
+//         if(arr[mid]==key)
+//         {
+//             printf("element found at position %d",mid+1);
+//             return 0;
+//         }
+//     else if(arr[mid]<key)
+//     {
+//         low=mid+1;
+//     }
+//     else
+//     {
+//        high=mid-1;
+//     }
+//  }
+//  printf("element not found");
+// }
+//  else
+//  {
+//     printf("invalid choice");
+//  }
+//  return 0;
+ 
+// }
+
+
+//quick sort
+
+// #include<stdio.h>
+// void swap(int *a,int *b)
+// {
+//     int temp=*a;
+//     *a=*b;
+//     *b=temp;
+// }
+
+// int partition(int arr[],int low,int high)
+// {
+//     int pivot = arr[high];
+//     int i=low-1;
+//     for(int j=low; j<high; j++)
+//     {
+//     if(arr[j]<=pivot)
+//     {
+//         i++;
+//        swap(&arr[i],&arr[j]);
+//     }
+// }
+//     swap(&arr[i+1],&arr[high]);
+//     return i+1;
+// }
+
+// void quicksort(int arr[],int low,int high)
+// {
+//     if(low<high){
+//     int pi = partition(arr,low,high);
+//     quicksort(arr,low,pi-1);
+//     quicksort(arr,pi+1,high);
+//     }
+// }
+
+// int main(){
+//     int n;
+//     printf("Enter number of elements:\n");
+//     scanf("%d",&n);
+
+//     int arr[n];
+
+//     printf("Enter %d elements:\n",n);
+//     for(int i=0; i<n; i++)
+//     {
+//         scanf("%d",&arr[i]);
+//     }
+//     quicksort(arr,0,n-1);
+//     printf("Sorted array:");
+//     for(int i=0; i<n; i++)
+//     {
+//        printf("%d",arr[i]);
+//     }
+   
+//     return 0;
+// }
+
+
+
+
+//
+
+
+// #include<stdio.h>
+
+// void swap(int *a,int *b)
+// {
+//     int temp = *a;
+//     *a=*b;
+//     *b=temp;
+// }
+
+// int partition(int arr[],int low ,int high)
+// {
+//     int pivot = arr[high];
+//     int i = low-1;
+//     for(int j=low; j<high; j++)
+// {
+        
+//     if(arr[j]<pivot)
+    
+//     {
+//         i++;
+//         swap(&arr[i],&arr[j]);
+//     }
+// }
+// swap(&arr[i+1],&arr[high]);
+// return i+1;
+// }
+
+// void quicksort(int arr[],int low,int high)
+// {
+//     if(low<high)
+//     {
+//         int pi = partition(arr,low,high);
+//         quicksort(arr,low,pi-1);
+//         quicksort(arr,high,pi+1);
+//     }
+// }
+
+// int main()
+// {
+//     int n;
+//     printf("Enter number of elements:\n");
+//     scanf("%d",&n);
+
+//     int arr[n];
+
+//     printf("Enter %d elements:\n",n);
+//     for(int i=0; i<n; i++)
+//     {
+//         scanf("%d",&arr[i]);
+//     }
+//     quicksort(arr,0,n-1);
+//     printf("Sorted array is :\n");
+//     for( int i=0; i<n; i++)
+//     {
+//         printf("%d",arr[i]);
+//     }
+//     return 0;
+// }
+
+//merge sort
+
 #include<stdio.h>
+
+void merge(int arr[],int left,int right,int mid)
+{
+    int i=left,j=mid+1,k=0;
+    int temp[50];
+    while (i<=mid && j<=right)
+    {
+       if(arr[i]<arr[j])
+       temp[k++]=arr[i++];
+       else
+       temp[k++]=arr[j++];
+    }
+    while(i<=mid)
+    temp[k++]=arr[i++];
+    while(j<=right)
+    temp[k++]=arr[j++];
+    for(i=left,k=0; i<=right; i++,k++)
+    arr[i]=temp[k];
+    
+}
+
+void mergesort(int arr[],int left,int right)
+{
+    if(left<right)
+    {
+        int mid = (left+right)/2;
+        mergesort(arr,mid-1,left);
+        mergesort(arr,mid+1,right);
+        merge(arr,left,right,mid);
+    }
+}
+
 int main()
 {
-    int i,n,key,choice;
-    printf("Enter number of elements : \n");
+    int arr[50],n;
+    printf("Enter number of elements:\n");
     scanf("%d",&n);
 
-    int arr[n];
-
-    printf("Enter %d elements:\n",n);
-    for(i=0; i<n; i++)
+    printf("Enter %d elements : \n",n);
+    for(int i=0; i<n; i++)
     {
         scanf("%d",&arr[i]);
     }
-
-    printf("-----search choice-----");
-    printf("1.Linear search");
-    printf("2.Binary search");
-    printf("Enter your choice:");
-    scanf("%d",&choice);
-    printf("Enter element  to serach :");
-    scanf("%d",&key);
-
-    if(choice==1)
+    mergesort(arr,0,n-1);
+    printf("sorted array is :\n");
+    for(int i=0; i<n; i++)
     {
-        for(i=0; i<n; i++)
-        {
-            if(arr[i]==key)
-            {
-                printf("Element found at position %d",i+1);
-                return 0;
-            }
-        }
-        printf("Element not found");
+        printf("%d",arr[i]);
     }
+    return 0;
 
-
-else if(choice==2)
-{
-    int low=0,high=n-1,mid;
-    while(low<=high)
-    {
-        mid=(low+high)/2;
-        if(arr[mid]==key)
-        {
-            printf("element found at position %d",mid+1);
-            return 0;
-        }
-    else if(arr[mid]<key)
-    {
-        low=mid+1;
-    }
-    else
-    {
-       high=mid-1;
-    }
- }
- printf("element not found");
 }
- else
- {
-    printf("invalid choice");
- }
- return 0;
- 
-}
+
+
+
+
+
 
 
 
