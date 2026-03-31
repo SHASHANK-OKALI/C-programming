cea341a4c714d0e680f57feaebaad1f9ba052bf3
Shashank-okali <shashankokali93@gmail.com>
2026-03-31 21:38:36 +0530

    practice

--- practice.c
+++ practice.c
@@ -2954,22 +2954,38 @@ int main() {
 
 //combination
 
+// #include <stdio.h>
+
+// int fact(int n) {
+//     int i, f = 1;
+//     for(i = 1; i <= n; i++)
+//         f *= i;
+//     return f;
+// }
+
+// int main() {
+//     int n, r;
+//     scanf("%d %d", &n, &r);
+
+//     int ncr = fact(n) / (fact(r) * fact(n - r));
+
+//     printf("nCr = %d", ncr);
+
+//     return 0;
+// }
+
+
+//even or odd 
+
 #include <stdio.h>
 
-int fact(int n) {
-    int i, f = 1;
-    for(i = 1; i <= n; i++)
-        f *= i;
-    return f;
-}
-
 int main() {
-    int n, r;
-    scanf("%d %d", &n, &r);
+    int total = 6;
+    int even = 3;
 
-    int ncr = fact(n) / (fact(r) * fact(n - r));
+    float prob = (float)even / total;
 
-    printf("nCr = %d", ncr);
+    printf("P(Even) = %.2f", prob);
 
     return 0;
 }
