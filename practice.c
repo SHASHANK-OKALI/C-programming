69da9de7c3daff77b40706f4bd43f0de8ebf4101
Shashank-okali <shashankokali93@gmail.com>
2026-04-21 23:41:03 +0530

    practice

--- practice.c
+++ practice.c
@@ -3419,6 +3419,26 @@ int main() {
 }
 
 
+#include <stdio.h>
+
+int main() {
+    int a[10][10], n, i, j, sum=0;
+
+    scanf("%d", &n);
+
+    for(i=0;i<n;i++)
+        for(j=0;j<n;j++)
+            scanf("%d",&a[i][j]);
+
+    for(i=0;i<n;i++)
+        sum += a[i][i];
+
+    printf("Sum = %d", sum);
+
+    return 0;
+}
+
+
 
 
 
