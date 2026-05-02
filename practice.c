55eb4bfa66d1f4369d0c96448a308783a2378c3a
Shashank-okali <shashankokali93@gmail.com>
2026-05-02 23:08:32 +0530

    practice

--- practice.c
+++ practice.c
@@ -3611,25 +3611,50 @@ int main() {
 
 
 
+// #include <stdio.h>
+
+// int main() {
+//     int a[10][10], r, c, i, j;
+
+//     scanf("%d %d", &r, &c);
+
+//     for(i=0;i<r;i++)
+//         for(j=0;j<c;j++)
+//             scanf("%d",&a[i][j]);
+
+//     for(j=0;j<c;j++) {
+//         int min = a[0][j];
+//         for(i=1;i<r;i++)
+//             if(a[i][j] < min)
+//                 min = a[i][j];
+
+//         printf("Column %d Min = %d\n", j+1, min);
+//     }
+
+//     return 0;
+// }
+
+
 #include <stdio.h>
 
 int main() {
-    int a[10][10], r, c, i, j;
+    int a[10][10], n, i, j, flag = 1;
 
-    scanf("%d %d", &r, &c);
+    scanf("%d", &n);
 
-    for(i=0;i<r;i++)
-        for(j=0;j<c;j++)
+    for(i=0;i<n;i++)
+        for(j=0;j<n;j++)
             scanf("%d",&a[i][j]);
 
-    for(j=0;j<c;j++) {
-        int min = a[0][j];
-        for(i=1;i<r;i++)
-            if(a[i][j] < min)
-                min = a[i][j];
+    for(i=1;i<n;i++)
+        for(j=0;j<i;j++)
+            if(a[i][j] != 0)
+                flag = 0;
 
-        printf("Column %d Min = %d\n", j+1, min);
-    }
+    if(flag)
+        printf("Upper Triangular");
+    else
+        printf("Not Upper Triangular");
 
     return 0;
 }
