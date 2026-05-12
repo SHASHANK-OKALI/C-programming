5c55ef42c24cb994f48fc8d708bf46781d7d0803
Shashank-okali <shashankokali93@gmail.com>
2026-05-12 23:57:49 +0530

    practice

--- practice.c
+++ practice.c
@@ -3724,21 +3724,40 @@ int main() {
 // }
 
 
+// #include <stdio.h>
+
+// int main() {
+//     int a[2][2], det;
+
+//     for(int i = 0; i < 2; i++)
+//         for(int j = 0; j < 2; j++)
+//             scanf("%d", &a[i][j]);
+
+//     det = a[0][0]*a[1][1] - a[0][1]*a[1][0];
+
+//     if(det == 0)
+//         printf("Singular Matrix");
+//     else
+//         printf("Non-Singular Matrix");
+
+//     return 0;
+// }
+
+
 #include <stdio.h>
 
 int main() {
-    int a[2][2], det;
+    int a[3][3], det;
 
-    for(int i = 0; i < 2; i++)
-        for(int j = 0; j < 2; j++)
+    for(int i = 0; i < 3; i++)
+        for(int j = 0; j < 3; j++)
             scanf("%d", &a[i][j]);
 
-    det = a[0][0]*a[1][1] - a[0][1]*a[1][0];
+    det = a[0][0]*(a[1][1]*a[2][2] - a[1][2]*a[2][1])
+        - a[0][1]*(a[1][0]*a[2][2] - a[1][2]*a[2][0])
+        + a[0][2]*(a[1][0]*a[2][1] - a[1][1]*a[2][0]);
 
-    if(det == 0)
-        printf("Singular Matrix");
-    else
-        printf("Non-Singular Matrix");
+    printf("Determinant = %d", det);
 
     return 0;
 }
