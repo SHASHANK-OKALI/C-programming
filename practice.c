075e80a5d4390196d11e6d1908925cc9ccc519d0
Shashank-okali <shashankokali93@gmail.com>
2026-06-19 22:20:57 +0530

    practice

--- practice.c
+++ practice.c
@@ -3985,6 +3985,28 @@ int main() {
 }
 
 
+#include <stdio.h>
+
+int main() {
+    int a[3][3], min, i, j;
+
+    for(i = 0; i < 3; i++)
+        for(j = 0; j < 3; j++)
+            scanf("%d", &a[i][j]);
+
+    min = a[0][0];
+
+    for(i = 0; i < 3; i++)
+        for(j = 0; j < 3; j++)
+            if(a[i][j] < min)
+                min = a[i][j];
+
+    printf("Minimum Cost = %d", min);
+
+    return 0;
+}
+
+
 
 
 
